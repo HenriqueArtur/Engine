25a2b6339e5f14eb43b04b17be3faef32279f03f
Henrique Artur <contato@henriqueartur.com>
2025-05-04 17:42:49 -0300

    💚 `conan` install dependencies

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,7 +7,7 @@ fi
 mkdir build
 
 # Install dependencies with Conan
-conan install . --output-folder=build --build=missing
+conan install . --output-folder=build --build=missing -c tools.system.package_manager:mode=install
 
 # Configure and build with CMake
 cmake -B build \
